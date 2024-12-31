6348643f3f0ad768ad8073c89978d30d851eeaf2
Pramit Gaha <pramitgaha@protonmail.com>
2024-12-31 10:21:29 +0545

    addresses struct defined, missing bitcoin address function

--- canisters/backend/src/lib.rs
+++ canisters/backend/src/lib.rs
@@ -1,5 +1,8 @@
+mod addresses;
+mod chain;
 mod memory;
 mod state;
+mod txn_handler;
 mod updater;
 
 use ic_cdk::api::management_canister::ecdsa::EcdsaPublicKeyResponse as EcdsaPublicKey;
@@ -10,5 +13,13 @@ pub fn init() {}
 pub fn pre_upgrade() {}
 
 pub fn post_upgrade() {}
+
+pub fn user_detail() {}
+
+pub fn all_launches() {}
+
+pub fn start_launch() {}
+
+pub fn participate() {}
 
 ic_cdk::export_candid!();

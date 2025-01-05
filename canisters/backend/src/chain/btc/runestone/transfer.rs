0798937f9970140fd632d1ab02c4e58c1f6e2c40
Pramit Gaha <pramitgaha@protonmail.com>
2025-01-05 16:28:52 +0545

    working on the marketplace

--- canisters/backend/src/chain/btc/runestone/transfer.rs
+++
@@ -1,9 +0,0 @@
-use crate::txn_handler::TransactionType;
-
-pub struct RuneTranferArgs {
-    runeid: u128,
-}
-
-pub fn transfer() -> Result<TransactionType, (u128, u64)> {
-    todo!()
-}
--- canisters/backend/src/state/user_manager.rs
+++
@@ -1,6 +0,0 @@
-use std::collections::HashSet;
-
-pub struct User {
-    pub launch_created: HashSet<u128>,
-    pub launch_participated: HashSet<u128>,
-}
